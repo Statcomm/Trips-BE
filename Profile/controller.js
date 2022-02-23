94d9568a50b6386ce8c62258c232c59f1fe10640
a1000rhan <1000rhan@gmail.com>
2022-02-23 09:03:09 +0300

    aa

--- Profile/controller.js
+++ Profile/controller.js
@@ -2,11 +2,21 @@ const { find } = require("../db/models/Profile");
 const Profile = require("../db/models/Profile");
 const Trip = require("../db/models/Trip");
 
+exports.fetchProfile = async (profileId, next) => {
+  try {
+    const profile = await Profile.findById(profileId);
+    return profile;
+  } catch (err) {
+    next(err);
+  }
+};
+
 exports.getProfile = async (req, res, next) => {
   try {
     const profiles = await Profile.find()
       .populate("owner trips")
       .populate({ path: "trips", populate: { path: "owner" } });
+
     res.status(201).json(profiles);
   } catch (e) {
     console.log(e);
@@ -43,6 +53,27 @@ exports.createTrip = async (req, res, next) => {
     );
     const profileTrip = await Profile.create({ trip: newTrip._id });
     return res.status(201).json(newTrip);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    // if (req.file) {
+    //   req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
+    // }
+    console.log(req.body); //new:true to to show the update after change immiditly
+    const profile = await Profile.findByIdAndUpdate(
+      { _id: req.profile.id },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.json(profile);
   } catch (err) {
     next(err);
   }
--- Profile/routes.js
+++ Profile/routes.js
@@ -3,17 +3,38 @@ const passport = require("passport");
 const upload = require("../middleware/multer");
 const routers = express.Router();
 
-const { getProfile, newProfile, createTrip } = require("./controller");
+const {
+  getProfile,
+  newProfile,
+  createTrip,
+  updateProfile,
+  fetchProfile,
+} = require("./controller");
+
+routers.param("profileId", async (req, res, next, id) => {
+  const profile = await fetchProfile(id, next);
+  if (profile) {
+    req.profile = profile;
+    next();
+  } else {
+    next({ status: 404, message: "trip not found" });
+  }
+});
 
 routers.get("/profiles", getProfile);
 
 routers.post(
-  "/profile",
+  "/profiles",
   passport.authenticate("jwt", { session: false }),
   newProfile
 );
+routers.put(
+  "/profiles/:profileId",
+  passport.authenticate("jwt", { session: false }),
+  updateProfile
+);
 routers.post(
-  "/:profileid/trips",
+  "/:profileId/trips",
   passport.authenticate("jwt", { session: false }),
   upload.single("image"),
   createTrip
